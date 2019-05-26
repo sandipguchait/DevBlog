5658d9c0de29c402595d94d08f975db98a46ffb3
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-26 15:19:49 +0530

    Adding endpoint and query and adding routes

--- DevBlog/static.config.js
+++ DevBlog/static.config.js
@@ -1,24 +1,61 @@
 import path from 'path';
+import { request } from 'graphql-request';
 
+const GRAPHCMS_ENDPOINT = 'https://api-apeast.graphcms.com/v1/cjw3nge0j1pip01etmht5upab/master';
+
+const QUERY = `
+{
+  posts{
+    title
+    id
+    image{
+      handle
+    }
+    content
+    tags
+    author{
+      name
+      id
+    }
+  }
+  
+  authors{
+    id
+    name
+    avatar{
+      handle
+    }
+    bio
+  }
+}
+`
 
 export default {
   getRoutes: async () => {
-    
+   const { posts, authors } =  await request(GRAPHCMS_ENDPOINT, QUERY)
 
     return [
       {
-        path: '/blog',
+        //this is the index route on pages directory
+        path: '/',
         getData: () => ({
           posts,
         }),
         children: posts.map(post => ({
           path: `/post/${post.id}`,
-          template: 'src/containers/Post',
+          template: 'src/pages/post',
           getData: () => ({
             post,
           }),
         })),
       },
+      {
+        path:'/about',
+        template: 'src/pages/about',
+        getData: () => ({
+          authors
+        }),
+      }
     ]
   },
   plugins: [
